ba454ce7813c5e7d317348eef80fd62b00ea180a
davort3x <vortex.da@gmail.com>
2025-04-20 18:58:33 +0200

    Update reading.js

--- routes/reading.js
+++ routes/reading.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Book = require('../../models/Book');
+const Book = require('../models/Book');
 
 // GET reading list page
 router.get('/', async (req, res) => {
