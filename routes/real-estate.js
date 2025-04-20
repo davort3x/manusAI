ab1f556477be71c29b856a7a34f29e5d0b25eac3
davort3x <vortex.da@gmail.com>
2025-04-20 18:58:23 +0200

    Update real-estate.js

--- routes/real-estate.js
+++ routes/real-estate.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Property = require('../../models/Property');
+const Property = require('../models/Property');
 
 // GET real estate page
 router.get('/', async (req, res) => {
