893d7f2febc8f79bdd8bcfd6553f7f2559afde47
davort3x <vortex.da@gmail.com>
2025-04-30 23:33:47 +0200

    Update dashboard.js

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // GET dashboard page - Redirects to /tasks
 router.get("/", (req, res) => {
-  res.redirect("/tasks"); // This line redirects to the tasks page
+  res.redirect("/tasks"); 
 });
 
 module.exports = router;
